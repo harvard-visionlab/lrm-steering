a95c2b9330540cd59967454e78f2cd175e265099
grez72 <grez72@gmail.com>
2023-12-12 05:31:16 -0600

    Create folder.py

---
+++ datasets/folder.py
@@ -0,0 +1,14 @@
+from torchvision.datasets import ImageFolder
+
+__all__ = ['ImageFolderIndex']
+
+class ImageFolderIndex(ImageFolder):
+    def __getitem__(self, index):
+        path, target = self.samples[index]
+        sample = self.loader(path)
+        if self.transform is not None:
+            sample = self.transform(sample)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return sample, target, index
