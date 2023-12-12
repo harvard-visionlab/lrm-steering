962bc4b495708a2a94e951009cf968a5d6b84d73
grez72 <grez72@gmail.com>
2023-12-12 06:07:20 -0600

    Update imagenette2.py

--- lrm_datasets/imagenette2.py
+++ lrm_datasets/imagenette2.py
@@ -1,6 +1,6 @@
 import os
 from .download import get_remote_data_file
-from .folder import ImageFolderIndex
+from .folder import ImagenetteDatasetRemapIN1K
 
 __all__ = ['imagenette2']
 
@@ -8,7 +8,7 @@ def imagenette2(split, transform=None, cache_dir=None):
   print("imagenette2", split, transform, cache_dir)
   url = "https://s3.amazonaws.com/fast-ai-imageclas/imagenette2-320.tgz"
   cached_filename, extracted_folder = get_remote_data_file(url, cache_dir=cache_dir)
-  dataset = ImageFolderIndex(os.path.join(extracted_folder, split), transform=transform)
+  dataset = ImagenetteDatasetRemapIN1K(os.path.join(extracted_folder, split), transform=transform)
   return dataset
 
   
